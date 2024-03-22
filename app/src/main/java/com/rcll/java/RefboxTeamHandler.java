0896a603914c30229f522190a642dfb6e9b07501
Peter Kohout <peter.kohout@student.tugraz.at>
2024-03-22 22:43:08 +0100

    implemented robot beacon signal via mqtt bridge.

--- app/src/main/java/com/rcll/java/RefboxTeamHandler.java
+++ app/src/main/java/com/rcll/java/RefboxTeamHandler.java
@@ -1,23 +1,22 @@
 package com.rcll.java;
 
-import com.google.protobuf.util.JsonFormat;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rcll.domain.*;
 import com.rcll.refbox.RefboxClient;
-import lombok.SneakyThrows;
 import lombok.extern.apachecommons.CommonsLog;
 import org.eclipse.paho.client.mqttv3.*;
-import org.robocup_logistics.llsf_msgs.MachineInstructionProtos;
 
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.Callable;
 import java.util.function.Consumer;
 
 @CommonsLog
 public class RefboxTeamHandler implements MqttCallback {
     private final IMqttClient mqttClient;
     private final RefboxClient refboxClient;
+
+    private final ObjectMapper objectMapper;
     private final Map<String, Consumer<String>> callbacks;
     private final String prepareBsInputTopic;
     private final String prepareBsOutputTopic;
@@ -28,11 +27,16 @@ public class RefboxTeamHandler implements MqttCallback {
     private final String prepareRs1Topic;
     private final String prepareRs2Topic;
 
+    private final String beaconRobot1Topic;
+    private final String beaconRobot2Topic;
+    private final String beaconRobot3Topic;
+
     public RefboxTeamHandler(IMqttClient mqttClient, RefboxClient refboxClient, String teamName) {
         this.mqttClient = mqttClient;
         this.refboxClient = refboxClient;
         this.mqttClient.setCallback(this);
         this.callbacks = new HashMap<>();
+        this.objectMapper = new ObjectMapper();
         prepareBsInputTopic = teamName + "/prepare/BS/input";
         prepareBsOutputTopic = teamName + "/prepare/BS/output";
         prepareCs1Topic = teamName + "/prepare/CS1";
@@ -40,6 +44,9 @@ public class RefboxTeamHandler implements MqttCallback {
         prepareDsTopic = teamName + "/prepare/DS";
         prepareRs1Topic = teamName + "/prepare/RS1";
         prepareRs2Topic = teamName + "/prepare/RS2";
+        beaconRobot1Topic = teamName + "/beacon/R1";
+        beaconRobot2Topic = teamName + "/beacon/R2";
+        beaconRobot3Topic = teamName + "/beacon/R3";
 
         this.callbacks.put(prepareBsInputTopic, this::prepareBsInput);
         this.callbacks.put(prepareBsOutputTopic, this::prepareBsOutput);
@@ -48,6 +55,9 @@ public class RefboxTeamHandler implements MqttCallback {
         this.callbacks.put(prepareDsTopic, this::prepareDs);
         this.callbacks.put(prepareRs1Topic, this::prepareRs1);
         this.callbacks.put(prepareRs2Topic, this::prepareRs2);
+        this.callbacks.put(beaconRobot1Topic, (s) -> this.sendRobotBeaconSignal(1, s));
+        this.callbacks.put(beaconRobot2Topic, (s) -> this.sendRobotBeaconSignal(2, s));
+        this.callbacks.put(beaconRobot3Topic, (s) -> this.sendRobotBeaconSignal(3, s));
     }
 
     private void prepareRs1(String s) {
@@ -90,6 +100,15 @@ public class RefboxTeamHandler implements MqttCallback {
         }
     }
 
+    private void sendRobotBeaconSignal(Integer robotId, String dataStr) {
+        try {
+            RobotBeaconData data = objectMapper.readValue(dataStr, RobotBeaconData.class);
+            this.refboxClient.sendBeaconSignal(robotId, data.getName(), data.getX(), data.getY(), data.getYaw());
+        } catch (Exception ex) {
+            log.warn("Error on sending Beacon signal for Robot" + robotId + "!", ex);
+        }
+    }
+
     private void prepareBsOutput(String s) {
         try {
             this.refboxClient.sendPrepareBS(MachineSide.Output, Base.valueOf(s));
@@ -107,7 +126,10 @@ public class RefboxTeamHandler implements MqttCallback {
     }
 
     public void start() throws MqttException {
-        this.mqttClient.subscribe(new String[]{prepareBsInputTopic, prepareBsOutputTopic, prepareCs1Topic, prepareCs2Topic, prepareDsTopic, prepareRs1Topic, prepareRs2Topic});
+        this.mqttClient.subscribe(new String[]{
+                prepareBsInputTopic, prepareBsOutputTopic, prepareCs1Topic,
+                prepareCs2Topic, prepareDsTopic, prepareRs1Topic, prepareRs2Topic,
+                beaconRobot1Topic, beaconRobot2Topic, beaconRobot3Topic});
     }
 
     @Override
---
+++ app/src/main/java/com/rcll/java/RobotBeaconData.java
@@ -0,0 +1,13 @@
+package com.rcll.java;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RobotBeaconData {
+    String name;
+    float x;
+    float y;
+    float yaw;
+}
